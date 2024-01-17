64428793afc630a8c5781882f5ce47ca803d4548
David Zemlin <dzemlin@asu.edu>
2024-01-16 22:10:34 -0500

    Build my first app in react using expo

---
+++ ExpoTestProgram/YarnTest/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hey Poste! iOS is here! It is time to get up!</Text>
+      <Text>Live Updating?</Text>
+      <Text>You Bet!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
