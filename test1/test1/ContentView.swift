4c0910c50b22e6b33aa0f086758f0a9030f95165
dzemlin <127574656+dzemlin@users.noreply.github.com>
2024-01-17 19:59:33 -0800

    set up some test programs
    set up some test programs

---
+++ test1/test1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test1
+//
+//  Created by user249626 on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
