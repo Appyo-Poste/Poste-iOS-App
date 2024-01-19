13e0dfd8051e59f903fa0523d426b493096735a6
dzemlin <127574656+dzemlin@users.noreply.github.com>
2024-01-18 23:41:01 -0500

    Creat base project for iOS from Apple OS with Xcode

---
+++ Tutorial1/Tutorial1/Tutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial1App.swift
+//  Tutorial1
+//
+//  Created by David Zemlin on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
